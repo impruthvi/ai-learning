1b4d066cadfd0ae1417862e39d32e5347e2c812f
Pruthvisinh Rajput <117271578+impruthvi13@users.noreply.github.com>
2023-10-03 17:07:41 +0530

    Implemented api for getting data from YT

--- app/api/chapters/route.ts
+++ app/api/chapters/route.ts
@@ -1,6 +1,8 @@
 // /api/chapter/getInto
 
 import { prisma } from "@/lib/db";
+import { strict_output } from "@/lib/gpt";
+import { getTranscript, searchYoutube } from "@/lib/youtube";
 
 import { NextResponse } from "next/server";
 import { z } from "zod";
@@ -31,7 +33,27 @@ export async function POST(req: Request, res: Response) {
       );
     }
 
-    return NextResponse.json({ success: true });
+    const videoId = await searchYoutube(chapter.youtubeSearchQuery);
+    let transcript = await getTranscript(videoId);
+    let maxLength = 500;
+    transcript = transcript.split(" ").slice(0, maxLength).join(" ");
+
+    const { summary }: { summary: string } = await strict_output(
+      "You are an AI capable of summarising a youtube transcript",
+      "summarise in 250 words or less and do not talk of the sponsors or anything unrelated to the main topic, also do not introduce what the summary is about.\n" +
+        transcript,
+      { summary: "summary of the transcript" }
+    );
+
+    await prisma.chapter.update({
+      where: { id: chapterId },
+      data: {
+        videoId,
+        summary,
+      },
+    });
+
+    return NextResponse.json({ videoId, transcript, summary });
   } catch (error) {
     if (error instanceof z.ZodError) {
       return NextResponse.json(
