f49df09460d7ed7c2470a243406ccf136e12acf2
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-10-03 13:31:20 +0530

    Implemented course create form

---
+++ app/(course)/create/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React from "react";
+import { redirect } from "next/navigation";
+import { InfoIcon } from "lucide-react";
+import { useAuth } from "@clerk/nextjs";
+import CreateCourseForm from "@/components/create-cource-form";
+
+type Props = {};
+
+const CreatePage =  (props: Props) => {
+  const { userId } = useAuth();
+
+  if (!userId) return redirect("/gallery");
+
+  return (
+    <div className="flex flex-col items-start max-w-xl px-8 mx-auto my-16 sm:px-0">
+      <h1 className="self-center text-3xl font-bold text-center sm:text-6xl">
+        Learning Journey
+      </h1>
+      <div className="flex p-4 mt-5 border-none bg-secondary">
+        <InfoIcon className="w-12 h-12 mr-3 text-blue-400" />
+        <div>
+          Enter in a course title, or what you want to learn about. Then enter a
+          list of units, which are the specifics you want to learn. And our AI
+          will generate a course for you!
+        </div>
+      </div>
+
+      <CreateCourseForm />
+    </div>
+  );
+};
+
+export default CreatePage;
