e42708dd286123f54664720cd82d5b2aeecbe0cd
Pruthvisinh Rajput <117271578+impruthvi13@users.noreply.github.com>
2023-10-05 18:15:47 +0530

    Update landing-hero.tsx

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import TypewriterComponent from "typewriter-effect";
 import { getAuthSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import { Button } from "./ui/button";
 
 export const LandingHero = async () => {
 
@@ -15,16 +15,7 @@ export const LandingHero = async () => {
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold ">
         <h1>The Best AI Tool for</h1>
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-500">
-          <TypewriterComponent
-            options={{
-              strings: [
-                "Creating Courses",
-                "Generating Content",
-              ],
-              autoStart: true,
-              loop: true,
-            }}
-          />
+          Creating Courses
         </div>
       </div>
 
@@ -34,14 +25,14 @@ export const LandingHero = async () => {
       </div>
 
       <div>
-        {/* <Link href={isSignedIn ? "/gallery" : "/sign-up"}>
+        <Link href={session?.user ? "/gallery" : "/sign-up"}>
           <Button
             variant="default"
             className="md:text-lg p-4 md:p-6 rounded-full font-semibold"
           >
             Start Generating For Free
           </Button>
-        </Link> */}
+        </Link>
       </div>
       <div className="text-zinc-400 text-xs md:text-sm font-normal">
         No credit card required.
